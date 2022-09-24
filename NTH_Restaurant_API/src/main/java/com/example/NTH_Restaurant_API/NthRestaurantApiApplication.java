e80ca46060175b549d603ccb703a268c6fd69a9e
thuha <thuhango0204@gmail.com>
2022-09-25 00:57:12 +0700

    first commit to create api restaurant manager

---
+++ NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/NthRestaurantApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.NTH_Restaurant_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NthRestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NthRestaurantApiApplication.class, args);
+	}
+
+}
---
+++ NTH_Restaurant_API/src/test/java/com/example/NTH_Restaurant_API/NthRestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NTH_Restaurant_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NthRestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
