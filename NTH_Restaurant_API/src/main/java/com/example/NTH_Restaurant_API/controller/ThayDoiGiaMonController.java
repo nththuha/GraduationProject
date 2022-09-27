fbd0412d111f27c2b16e18e9a9efb7bb4ef75dec
thuha <thuhango0204@gmail.com>
2022-09-27 21:01:02 +0700

    update thay đổi giá món controller

--- NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/controller/ThayDoiGiaMonController.java
+++ NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/controller/ThayDoiGiaMonController.java
@@ -1,11 +1,37 @@
 package com.example.NTH_Restaurant_API.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.NTH_Restaurant_API.dto.ThayDoiGiaMonDTO;
+import com.example.NTH_Restaurant_API.service.ThayDoiGiaMonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/thaydoigiamon")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ThayDoiGiaMonController {
+    @Autowired
+    private ThayDoiGiaMonService thayDoiGiaMonService;
+
+    @GetMapping("/{mama}")
+    public List<ThayDoiGiaMonDTO> layDSGiaTheoMon(@PathVariable String mama) {
+        return thayDoiGiaMonService.layDSGiaMon(mama);
+    }
+
+    @PutMapping("")
+    public String suaGiaMon(@Valid @RequestBody ThayDoiGiaMonDTO thayDoiGiaMonDTO) {
+        return thayDoiGiaMonService.suaGiaMon(thayDoiGiaMonDTO);
+    }
+
+    @PostMapping("")
+    public String themGiaMon(@Valid @RequestBody ThayDoiGiaMonDTO thayDoiGiaMonDTO) {
+        return thayDoiGiaMonService.themGiaMon(thayDoiGiaMonDTO);
+    }
+
+    @DeleteMapping("/{idGM}")
+    public String xoaGiaMon(@PathVariable Integer idGM) {
+        return thayDoiGiaMonService.xoaGiaMon(idGM);
+    }
 }
