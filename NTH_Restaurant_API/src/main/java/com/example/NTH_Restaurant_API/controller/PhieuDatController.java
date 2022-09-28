47c43f8fda66eaca1a90bdf618c260aff99c4a61
thuha <thuhango0204@gmail.com>
2022-09-28 21:42:52 +0700

    create phiếu đặt controller

--- NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/controller/PhieuDatController.java
+++ NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/controller/PhieuDatController.java
@@ -1,11 +1,24 @@
 package com.example.NTH_Restaurant_API.controller;
 
+import com.example.NTH_Restaurant_API.dto.PhieuDatDTO;
+import com.example.NTH_Restaurant_API.service.PhieuDatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/phieudat")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class PhieuDatController {
+    @Autowired
+    private PhieuDatService phieuDatService;
+
+    @GetMapping("")
+    public List<PhieuDatDTO> layDSPhieuDatChuaCoHoaDon() {
+        return phieuDatService.layDSPhieuDatChuaCoHoaDon();
+    }
 }
