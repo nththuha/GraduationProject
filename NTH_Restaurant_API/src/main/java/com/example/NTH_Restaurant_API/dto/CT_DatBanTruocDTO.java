067680f473d82debb6aca918e00e71fa5d63ce05
thuha <thuhango0204@gmail.com>
2022-10-10 22:25:57 +0700

    update dto

--- NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/dto/CT_DatBanTruocDTO.java
+++ NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/dto/CT_DatBanTruocDTO.java
@@ -1,9 +1,4 @@
 package com.example.NTH_Restaurant_API.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 public class CT_DatBanTruocDTO {
 }
