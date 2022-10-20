56e084d8dd5cb5b9aecbee4032afe94d0a2b5caf
thuha <thuhango0204@gmail.com>
2022-10-20 17:59:44 +0700

    quản lý khách hàng - done

--- NTHRestaurantManager/NTH Restaurant Manager/NTH Restaurant Manager/QuanLy/HeThong/frmKhachHang.Designer.cs
+++ NTHRestaurantManager/NTH Restaurant Manager/NTH Restaurant Manager/QuanLy/HeThong/frmKhachHang.Designer.cs
@@ -30,6 +30,9 @@ namespace NTH_Restaurant_Manager
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label hoTenLabel;
+            System.Windows.Forms.Label cmndLabel;
+            System.Windows.Forms.Label sdtLabel;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmKhachHang));
             this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
             this.bar2 = new DevExpress.XtraBars.Bar();
@@ -44,8 +47,60 @@ namespace NTH_Restaurant_Manager
             this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
             this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
             this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.bdsKH = new System.Windows.Forms.BindingSource(this.components);
+            this.gcKH = new DevExpress.XtraGrid.GridControl();
+            this.gvKH = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colhoTen = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colcmnd = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colsdt = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.txt_SDT = new DevExpress.XtraEditors.TextEdit();
+            this.txt_CMND = new DevExpress.XtraEditors.TextEdit();
+            this.txt_HoTen = new DevExpress.XtraEditors.TextEdit();
+            this.label1 = new System.Windows.Forms.Label();
+            hoTenLabel = new System.Windows.Forms.Label();
+            cmndLabel = new System.Windows.Forms.Label();
+            sdtLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsKH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcKH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvKH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_SDT.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_CMND.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_HoTen.Properties)).BeginInit();
             this.SuspendLayout();
+            // 
+            // hoTenLabel
+            // 
+            hoTenLabel.AutoSize = true;
+            hoTenLabel.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            hoTenLabel.Location = new System.Drawing.Point(24, 70);
+            hoTenLabel.Name = "hoTenLabel";
+            hoTenLabel.Size = new System.Drawing.Size(106, 27);
+            hoTenLabel.TabIndex = 9;
+            hoTenLabel.Text = "Họ và tên";
+            // 
+            // cmndLabel
+            // 
+            cmndLabel.AutoSize = true;
+            cmndLabel.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            cmndLabel.Location = new System.Drawing.Point(24, 126);
+            cmndLabel.Name = "cmndLabel";
+            cmndLabel.Size = new System.Drawing.Size(83, 27);
+            cmndLabel.TabIndex = 10;
+            cmndLabel.Text = "CMND";
+            // 
+            // sdtLabel
+            // 
+            sdtLabel.AutoSize = true;
+            sdtLabel.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            sdtLabel.Location = new System.Drawing.Point(24, 181);
+            sdtLabel.Name = "sdtLabel";
+            sdtLabel.Size = new System.Drawing.Size(137, 27);
+            sdtLabel.TabIndex = 11;
+            sdtLabel.Text = "Số điện thoại";
             // 
             // barManager1
             // 
@@ -93,6 +148,7 @@ namespace NTH_Restaurant_Manager
             this.btn_Them.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Them.ImageOptions.SvgImage")));
             this.btn_Them.Name = "btn_Them";
             this.btn_Them.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_Them.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_Them_ItemClick);
             // 
             // btn_CapNhat
             // 
@@ -101,6 +157,7 @@ namespace NTH_Restaurant_Manager
             this.btn_CapNhat.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_CapNhat.ImageOptions.SvgImage")));
             this.btn_CapNhat.Name = "btn_CapNhat";
             this.btn_CapNhat.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_CapNhat.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_CapNhat_ItemClick);
             // 
             // btn_Luu
             // 
@@ -110,6 +167,7 @@ namespace NTH_Restaurant_Manager
             this.btn_Luu.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Luu.ImageOptions.SvgImage")));
             this.btn_Luu.Name = "btn_Luu";
             this.btn_Luu.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_Luu.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_Luu_ItemClick);
             // 
             // btn_Xoa
             // 
@@ -118,6 +176,7 @@ namespace NTH_Restaurant_Manager
             this.btn_Xoa.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Xoa.ImageOptions.SvgImage")));
             this.btn_Xoa.Name = "btn_Xoa";
             this.btn_Xoa.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_Xoa.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_Xoa_ItemClick);
             // 
             // btn_PhucHoi
             // 
@@ -126,6 +185,7 @@ namespace NTH_Restaurant_Manager
             this.btn_PhucHoi.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_PhucHoi.ImageOptions.SvgImage")));
             this.btn_PhucHoi.Name = "btn_PhucHoi";
             this.btn_PhucHoi.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_PhucHoi.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_PhucHoi_ItemClick);
             // 
             // btn_Reload
             // 
@@ -134,6 +194,7 @@ namespace NTH_Restaurant_Manager
             this.btn_Reload.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Reload.ImageOptions.SvgImage")));
             this.btn_Reload.Name = "btn_Reload";
             this.btn_Reload.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_Reload.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_Reload_ItemClick);
             // 
             // btn_Thoat
             // 
@@ -142,6 +203,7 @@ namespace NTH_Restaurant_Manager
             this.btn_Thoat.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Thoat.ImageOptions.SvgImage")));
             this.btn_Thoat.Name = "btn_Thoat";
             this.btn_Thoat.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_Thoat.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_Thoat_ItemClick);
             // 
             // barDockControlTop
             // 
@@ -175,11 +237,130 @@ namespace NTH_Restaurant_Manager
             this.barDockControlRight.Manager = this.barManager1;
             this.barDockControlRight.Size = new System.Drawing.Size(0, 690);
             // 
+            // bdsKH
+            // 
+            this.bdsKH.DataSource = typeof(NTH_Restaurant_Manager.Model.KhachHangModel);
+            // 
+            // gcKH
+            // 
+            this.gcKH.DataSource = this.bdsKH;
+            this.gcKH.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gcKH.Location = new System.Drawing.Point(0, 30);
+            this.gcKH.MainView = this.gvKH;
+            this.gcKH.MenuManager = this.barManager1;
+            this.gcKH.Name = "gcKH";
+            this.gcKH.Size = new System.Drawing.Size(1446, 362);
+            this.gcKH.TabIndex = 5;
+            this.gcKH.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvKH});
+            // 
+            // gvKH
+            // 
+            this.gvKH.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colhoTen,
+            this.colcmnd,
+            this.colsdt});
+            this.gvKH.GridControl = this.gcKH;
+            this.gvKH.Name = "gvKH";
+            this.gvKH.OptionsBehavior.Editable = false;
+            this.gvKH.RowClick += new DevExpress.XtraGrid.Views.Grid.RowClickEventHandler(this.gvKH_RowClick);
+            // 
+            // colhoTen
+            // 
+            this.colhoTen.Caption = "HỌ TÊN KHÁCH HÀNG";
+            this.colhoTen.FieldName = "hoTen";
+            this.colhoTen.MinWidth = 25;
+            this.colhoTen.Name = "colhoTen";
+            this.colhoTen.Visible = true;
+            this.colhoTen.VisibleIndex = 0;
+            this.colhoTen.Width = 94;
+            // 
+            // colcmnd
+            // 
+            this.colcmnd.Caption = "CMND";
+            this.colcmnd.FieldName = "cmnd";
+            this.colcmnd.MinWidth = 25;
+            this.colcmnd.Name = "colcmnd";
+            this.colcmnd.Visible = true;
+            this.colcmnd.VisibleIndex = 1;
+            this.colcmnd.Width = 94;
+            // 
+            // colsdt
+            // 
+            this.colsdt.Caption = "SỐ ĐIỆN THOẠI";
+            this.colsdt.FieldName = "sdt";
+            this.colsdt.MinWidth = 25;
+            this.colsdt.Name = "colsdt";
+            this.colsdt.Visible = true;
+            this.colsdt.VisibleIndex = 2;
+            this.colsdt.Width = 94;
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(sdtLabel);
+            this.panelControl2.Controls.Add(this.txt_SDT);
+            this.panelControl2.Controls.Add(cmndLabel);
+            this.panelControl2.Controls.Add(this.txt_CMND);
+            this.panelControl2.Controls.Add(hoTenLabel);
+            this.panelControl2.Controls.Add(this.txt_HoTen);
+            this.panelControl2.Controls.Add(this.label1);
+            this.panelControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl2.Enabled = false;
+            this.panelControl2.Location = new System.Drawing.Point(0, 392);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(1446, 328);
+            this.panelControl2.TabIndex = 6;
+            // 
+            // txt_SDT
+            // 
+            this.txt_SDT.DataBindings.Add(new System.Windows.Forms.Binding("EditValue", this.bdsKH, "sdt", true));
+            this.txt_SDT.Location = new System.Drawing.Point(203, 178);
+            this.txt_SDT.MenuManager = this.barManager1;
+            this.txt_SDT.Name = "txt_SDT";
+            this.txt_SDT.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_SDT.Properties.Appearance.Options.UseFont = true;
+            this.txt_SDT.Size = new System.Drawing.Size(564, 32);
+            this.txt_SDT.TabIndex = 12;
+            // 
+            // txt_CMND
+            // 
+            this.txt_CMND.DataBindings.Add(new System.Windows.Forms.Binding("EditValue", this.bdsKH, "cmnd", true));
+            this.txt_CMND.Location = new System.Drawing.Point(203, 123);
+            this.txt_CMND.MenuManager = this.barManager1;
+            this.txt_CMND.Name = "txt_CMND";
+            this.txt_CMND.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_CMND.Properties.Appearance.Options.UseFont = true;
+            this.txt_CMND.Size = new System.Drawing.Size(564, 32);
+            this.txt_CMND.TabIndex = 11;
+            // 
+            // txt_HoTen
+            // 
+            this.txt_HoTen.DataBindings.Add(new System.Windows.Forms.Binding("EditValue", this.bdsKH, "hoTen", true));
+            this.txt_HoTen.Location = new System.Drawing.Point(203, 67);
+            this.txt_HoTen.MenuManager = this.barManager1;
+            this.txt_HoTen.Name = "txt_HoTen";
+            this.txt_HoTen.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_HoTen.Properties.Appearance.Options.UseFont = true;
+            this.txt_HoTen.Size = new System.Drawing.Size(564, 32);
+            this.txt_HoTen.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 35);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "QUẢN LÝ KHÁCH HÀNG";
+            // 
             // frmKhachHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1446, 720);
+            this.Controls.Add(this.panelControl2);
+            this.Controls.Add(this.gcKH);
             this.Controls.Add(this.barDockControlLeft);
             this.Controls.Add(this.barDockControlRight);
             this.Controls.Add(this.barDockControlBottom);
@@ -187,6 +368,15 @@ namespace NTH_Restaurant_Manager
             this.Name = "frmKhachHang";
             this.Text = "KHÁCH HÀNG";
             ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsKH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcKH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvKH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            this.panelControl2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_SDT.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_CMND.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_HoTen.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,5 +397,16 @@ namespace NTH_Restaurant_Manager
         private DevExpress.XtraBars.BarDockControl barDockControlBottom;
         private DevExpress.XtraBars.BarDockControl barDockControlLeft;
         private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraGrid.GridControl gcKH;
+        private System.Windows.Forms.BindingSource bdsKH;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvKH;
+        private DevExpress.XtraGrid.Columns.GridColumn colhoTen;
+        private DevExpress.XtraGrid.Columns.GridColumn colcmnd;
+        private DevExpress.XtraGrid.Columns.GridColumn colsdt;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.TextEdit txt_SDT;
+        private DevExpress.XtraEditors.TextEdit txt_CMND;
+        private DevExpress.XtraEditors.TextEdit txt_HoTen;
     }
 }
--- NTHRestaurantManager/NTH Restaurant Manager/NTH Restaurant Manager/QuanLy/HeThong/frmKhachHang.cs
+++ NTHRestaurantManager/NTH Restaurant Manager/NTH Restaurant Manager/QuanLy/HeThong/frmKhachHang.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NTH_Restaurant_Manager.Model;
+using NTH_Restaurant_Manager.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,161 @@ namespace NTH_Restaurant_Manager
 {
     public partial class frmKhachHang : Form
     {
+        KhachHangRepository _repository = new KhachHangRepository();
+        KhachHangModel khachHang;
+        int idKH;
+        String button;
+
         public frmKhachHang()
         {
             InitializeComponent();
+            khoiTao();
+            layDSKhachHang();
+        }
+
+        private void khoiTao()
+        {
+            txt_HoTen.Text = "";
+            txt_CMND.Text = "";
+            txt_SDT.Text = "";
+        }
+
+        private async void layDSKhachHang()
+        {
+            try
+            {
+                var listKH = await _repository.layDSKhachHang();
+                gcKH.DataSource = listKH;
+                if(listKH.Count > 0)
+                {
+                    setGiaTri(0);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Lỗi load danh sách khách hàng: " + e.Message, "Thông báo");
+            }
+        }
+
+        private void setGiaTri(int num)
+        {
+            idKH = int.Parse(gvKH.GetRowCellValue(num, "idKH").ToString());
+            txt_HoTen.Text = gvKH.GetRowCellValue(num, "hoTen").ToString();
+            txt_CMND.Text = gvKH.GetRowCellValue(num, "cmnd").ToString();
+            txt_SDT.Text = gvKH.GetRowCellValue(num, "sdt").ToString();
+        }
+
+        private void gvKH_RowClick(object sender, DevExpress.XtraGrid.Views.Grid.RowClickEventArgs e)
+        {
+            setGiaTri(e.RowHandle);
+        }
+
+        private void btn_Thoat_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Reload_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            layDSKhachHang();
+        }
+
+        private void btn_PhucHoi_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            bdsKH.CancelEdit();
+            layDSKhachHang();
+            gcKH.Enabled = false;
+            panelControl2.Enabled = btn_Them.Enabled = btn_Xoa.Enabled = btn_CapNhat.Enabled = btn_Thoat.Enabled = btn_Reload.Enabled = true;
+            btn_PhucHoi.Enabled = btn_Luu.Enabled = false;
+        }
+
+        private async void themKhachHang()
+        {
+            String check = await _repository.themKhachHang(khachHang);
+            if (check.Equals("false")) MessageBox.Show("Thêm khách hàng thất bại!", "Thông báo");
+            else MessageBox.Show("Thêm khách hàng thành công!", "Thông báo");
+            layDSKhachHang();
+        }
+
+        private async void suaKhachHang()
+        {
+            String check = await _repository.suaKhachHang(khachHang);
+            if (check.Equals("false")) MessageBox.Show("Cập nhật khách hàng thất bại!", "Thông báo");
+            else MessageBox.Show("Cập nhật khách hàng thành công!", "Thông báo");
+            layDSKhachHang();
+        }
+
+        private async void xoaKhachHang()
+        {
+            String check = await _repository.xoaKhachHang(idKH);
+            if (check.Equals("false")) MessageBox.Show("Xóa khách hàng thất bại!", "Thông báo");
+            else MessageBox.Show("Xóa khách hàng thành công!", "Thông báo");
+            layDSKhachHang();
+        }
+
+        private void btn_Them_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            button = "Thêm";
+            khoiTao();
+            gcKH.Enabled = btn_Them.Enabled = btn_CapNhat.Enabled = btn_Reload.Enabled = btn_Xoa.Enabled = false;
+            btn_Luu.Enabled = btn_PhucHoi.Enabled = true;
+            panelControl2.Enabled = true;
+        }
+
+        private void btn_CapNhat_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            button = "Cập nhật";
+            gcKH.Enabled = btn_Them.Enabled = btn_CapNhat.Enabled = btn_Reload.Enabled = btn_Xoa.Enabled = false;
+            btn_Luu.Enabled = btn_PhucHoi.Enabled = true;
+            panelControl2.Enabled = true;
+        }
+
+        private void btn_Xoa_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            String hoTen = txt_HoTen.Text;
+            if (MessageBox.Show("Bạn có thật sự muốn xóa khách hàng " + hoTen + "?", "Xác nhận", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                xoaKhachHang();
+                khoiTao();
+            }
+        }
+
+        private void btn_Luu_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (txt_HoTen.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("Họ tên không được để trống", "Thông báo", MessageBoxButtons.OK);
+                txt_HoTen.Focus();
+                return;
+            }
+            if (txt_CMND.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("CMND không được để trống", "Thông báo", MessageBoxButtons.OK);
+                txt_CMND.Focus();
+                return;
+            }
+            if (txt_SDT.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("Số điện thoại không được để trống", "Thông báo", MessageBoxButtons.OK);
+                txt_SDT.Focus();
+                return;
+            }
+            khachHang = new KhachHangModel();
+            khachHang.hoTen = txt_HoTen.Text.Trim();
+            khachHang.cmnd = txt_CMND.Text.Trim();
+            khachHang.sdt = txt_SDT.Text.Trim();
+            if (button.Equals("Thêm"))
+            {
+                themKhachHang();
+            }
+            else
+            {
+                khachHang.idKH = idKH;
+                suaKhachHang();
+            }
+            gcKH.Enabled = btn_Them.Enabled = btn_CapNhat.Enabled = btn_Reload.Enabled = btn_Xoa.Enabled = true;
+            btn_Luu.Enabled = btn_PhucHoi.Enabled = false;
+            panelControl2.Enabled = false;
         }
     }
 }
