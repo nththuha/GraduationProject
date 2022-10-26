8c2ee9cbbb6dfc0abc744a2425cf0c4f7b66dad6
thuha <thuhango0204@gmail.com>
2022-10-26 22:24:45 +0700

    cũng không biết đang làm gì luôn :))

--- NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/controller/CT_BanController.java
+++ NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/controller/CT_BanController.java
@@ -27,7 +27,7 @@ public class CT_BanController {
         return ct_banService.layDSCTBanTheoPhong(maphong);
     }
 
-    @GetMapping("/dattruoc")
+    @PostMapping("/dattruoc")
     public List<BanDTO> layDSBanTheoPhongTheoNgay(@Valid @RequestBody PhongNgay phongNgay){
         return ct_banService.layDSBanTheoPhongTheoNgay(phongNgay);
     }
