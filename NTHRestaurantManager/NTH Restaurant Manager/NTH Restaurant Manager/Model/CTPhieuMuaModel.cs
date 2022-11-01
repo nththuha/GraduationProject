9b1859a14c3b6c2dcd490841a7a4f6adedc58017
thuha <thuhango0204@gmail.com>
2022-11-02 00:32:33 +0700

    update winform

---
+++ NTHRestaurantManager/NTH Restaurant Manager/NTH Restaurant Manager/Model/CTPhieuMuaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTH_Restaurant_Manager.Model
+{
+    class CTPhieuMuaModel
+    {
+        public int idCTPM { get; set; }
+        public int soLuong { get; set; }
+        public String manl { get; set; }
+        public String tennl { get; set; }
+        public String donVi { get; set; }
+        public int idpm { get; set; }
+    }
+}
---
+++ NTHRestaurantManager/NTH Restaurant Manager/NTH Restaurant Manager/Model/PhieuMuaNguyenLieuModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTH_Restaurant_Manager.Model
+{
+    class PhieuMuaNguyenLieuModel
+    {
+        public int idPM { get; set; }
+        public String ngay { get; set; }
+        public int idnv { get; set; }
+        public String hoTenNV { get; set; }
+    }
+}
