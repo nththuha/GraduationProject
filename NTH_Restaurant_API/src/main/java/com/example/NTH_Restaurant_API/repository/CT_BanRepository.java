d5ad94b5f8babb4be5cc13031d22ce88f5b4ada8
thuha <thuhango0204@gmail.com>
2022-11-06 00:34:06 +0700

    update lấy chi tiết bàn đã đặt của phiếu đặt trước

--- NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/repository/CT_BanRepository.java
+++ NTH_Restaurant_API/src/main/java/com/example/NTH_Restaurant_API/repository/CT_BanRepository.java
@@ -28,13 +28,11 @@ public interface CT_BanRepository extends JpaRepository<CT_BanEntity, Integer> {
     @Modifying
     @Query(value = "select t.idctb, t.maphong, t.maban \n" +
             "from (select idpdt \n" +
-            "\t\tfrom phieudattruoc \n" +
-            "\t\twhere ngaydat = :ngay \n" +
-            "\t\t\tand (@DATE_PART('hour', giodat::timestamp - :gio::timestamp) < 3 \n" +
-            "\t\t\tor @DATE_PART('hour',:gio::timestamp - giodat::timestamp) < 3)) pdt, \n" +
-            "(select ctb.idctb, ctb.maban, ctb.maphong, dbt.idpdt \n" +
+            "from phieudattruoc \n" +
+            "where ngaydat = :ngay and giodat - 3 < :gio and giodat + 3 > :gio) pdt,\n" +
+            "(select ctb.idctb, ctb.maban, ctb.maphong, dbt.idpdt\n" +
             "from ct_ban ctb, ct_datbantruoc dbt \n" +
             "where ctb.idctb = dbt.idctb and ctb.maphong = :maPhong) t \n" +
             "where pdt.idpdt = t.idpdt", nativeQuery = true)
-    List<BanDaDat> layCTBanDaDat(@Param("ngay") Date ngay, @Param("gio") Date gio, @Param("maPhong") String maPhong);
+    List<BanDaDat> layCTBanDaDat(@Param("ngay") Date ngay, @Param("gio") Integer gio, @Param("maPhong") String maPhong);
 }
